974c0c7bab5c877e72a300a8580d0131ff4916f6
Daazzer <w513765485@163.com>
2024-01-22 21:32:02 +0800

    feat: 改为箭头函数

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:id', (req, res) => {
+  res.send(req.params.id);
+});
+
+router.get('/my/edit', (req, res) => {
+  res.end('waaa');
+});
+
 module.exports = router;
