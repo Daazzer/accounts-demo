45ef7e5b7191c019c152d259346425430ead0c28
Daazzer <w513765485@163.com>
2024-01-24 22:59:18 +0800

    feat: 初始数据请求

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,11 @@ router.get('/account', (req, res) => {
   res.render('account', { title: '记账本' });
 });
 
+router.post('/account', (req, res) => {
+  console.log(req.body);
+  res.send('添加记录');
+});
+
 router.get('/account/create', (req, res) => {
   res.render('account-create', { title: '添加记录' });
 });
