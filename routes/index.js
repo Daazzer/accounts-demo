dd166ac512dab9883f67d96fb61f059eeba8bab0
Daazzer <w513765485@163.com>
2024-01-25 21:21:01 +0800

    feat: 使用 lowdb

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync(__dirname + '/../data/db.json');
+const db = low(adapter);
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -12,6 +16,8 @@ router.get('/account', (req, res) => {
 
 router.post('/account', (req, res) => {
   console.log(req.body);
+  // 写入文件
+  db.get('accounts').push(req.body).write();
   res.send('添加记录');
 });
 
