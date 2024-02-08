f3a023a7244bbb9d6cdd68eb5b4ee7fe0f30421c
Daazzer <w513765485@163.com>
2024-02-08 17:38:24 +0800

    feat: 注册页

--- routes/web/auth.js
+++ routes/web/auth.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const UserModel = require('../../models/UserModel');
+const md5 = require('md5');
 
 router.get('/reg', (req, res) => {
   res.render('auth/reg', { basedir: 'views', title: '注册' });
+});
+
+router.post('/reg', (req, res) => {
+  UserModel.create({ ...req.body, password: md5(req.body.password) }, err => {
+    if (err) {
+      return res.status(500).send('注册失败');
+    }
+
+    res.render('success', { title: '注册成功', url: '/login' });
+  });
 });
 
 module.exports = router;
